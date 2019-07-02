137905da91fb89435ce875aaba2c24ca670ef4be
Christien <ckjohn@sfu.ca>
2019-07-01 19:18:52 -0700

    first draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  #get 'home/index'
   devise_for :users
-  devise_scope :user do
-    get 'register', to: 'devise/registrations#new', as: :register
-    get 'login', to: 'devise/sessions#new', as: :login
-  end
+  #root "home#index"
+  #as :user do
+  #get 'sign_in', :to => 'devise/registrations#new'
+  #end
+
+  #devise_scope :user do
+  #  get 'register', to: 'devise/registrations#new', as: :register
+  #  get 'login', to: 'devise/sessions#new', as: :login
+  #end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
