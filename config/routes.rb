0a077d1889f77ddfefe4c36e7a057e784024e35d
Christien <ckjohn@sfu.ca>
2019-07-01 18:31:43 -0700

    heroku sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  #get 'home/index'
   devise_for :users
-  #root "home#index"
-  as :user do
-  get 'sign_in', :to => 'devise/sessions#new'
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
