a4222469596e3fc5083120b1cfbca65964429297
Christien <ckjohn@sfu.ca>
2019-07-01 18:07:50 -0700

    Sign up sheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  #get 'home/index'
   devise_for :users
-  root to: "home#index"
+  #root "home#index"
+  as :user do
+  get 'sign_in', :to => 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
