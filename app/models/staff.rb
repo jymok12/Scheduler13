70f1844244f4dbca4257a86d2daef0efb0092824
Christien <ckjohn@sfu.ca>
2019-07-20 20:19:43 -0700

    heroku fix

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -4,5 +4,7 @@ class Staff < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  def username
+    return self.email.split('@')[0].capitalize
+  end
 end
